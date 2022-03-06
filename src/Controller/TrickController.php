5292d4efedbe1d0fe69a058431f35d7613d713fc
Reybeka Peyrat <reybekapeyrat@gmail.com>
2022-03-07 00:53:20 +0100

    add trick crud

---
+++ src/Controller/TrickController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Trick;
+use App\Form\TrickType;
+use App\Repository\TrickRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/trick')]
+class TrickController extends AbstractController
+{
+    #[Route('/', name: 'trick_index', methods: ['GET'])]
+    public function index(TrickRepository $trickRepository): Response
+    {
+        return $this->render('trick/index.html.twig', [
+            'tricks' => $trickRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'trick_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $trick = new Trick();
+        $form = $this->createForm(TrickType::class, $trick);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($trick);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('trick_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('trick/new.html.twig', [
+            'trick' => $trick,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'trick_show', methods: ['GET'])]
+    public function show(Trick $trick): Response
+    {
+        return $this->render('trick/show.html.twig', [
+            'trick' => $trick,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'trick_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Trick $trick, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(TrickType::class, $trick);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('trick_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('trick/edit.html.twig', [
+            'trick' => $trick,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'trick_delete', methods: ['POST'])]
+    public function delete(Request $request, Trick $trick, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$trick->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($trick);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('trick_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/TrickType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Trick;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrickType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('TrickName', \Symfony\Component\Form\Extension\Core\Type\TextType::class)
+            ->add('createdAt')
+            ->add('category')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Trick::class,
+        ]);
+    }
+}
